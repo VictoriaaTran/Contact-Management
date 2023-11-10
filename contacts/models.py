7604ddaf3b4a3c4b58ec0e307733d51c12b7d19e
VictoriaaTran <victoriatrnn@gmail.com>
2023-11-10 11:48:52 -0800

    added db models

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=20, null=False, unique=True, error_messages={'unique':"Name has already been taken"})
+    email = models.EmailField(null=False, unique=True, error_messages={'unique':"Invalid email"})
+    created_time = models.DateTimeField(auto_now_add=True)
---
+++ contacts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path 
+
+from .views import (
+    ContactListView
+)
+
+urlpatterns = [
+    path("", ContactListView.as_view(), name="contact_list"),
+]
