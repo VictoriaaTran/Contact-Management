b5da1cfe7181ebcd0c2d3e8a7fca650f83cef69c
VictoriaaTran <victoriatrnn@gmail.com>
2023-11-10 22:03:37 -0800

    added contact form template

--- contacts/urls.py
+++ contacts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path 
 
 from .views import (
-    ContactListView
+    ContactListView,
+    ContactCreateView
 )
 
 urlpatterns = [
     path("", ContactListView.as_view(), name="contact_list"),
+    path("create/", ContactCreateView.as_view(), name="contact_create")
 ]
