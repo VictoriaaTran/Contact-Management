78a63b5a7f9a27bace536a83622e4f744828744f
VictoriaaTran <victoriatrnn@gmail.com>
2023-11-10 21:36:35 -0800

    added form and form view

--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 from .models import Contact
+from .contact_form import ContactForm
 
 #listing of all contacts
 class ContactListView(ListView):
     model = Contact
 
+class ContactCreateView(CreateView):
+    model = Contact
+    form_class = ContactForm
+
+    #returns to homepage when contact is created successfully
+    success_url = reverse_lazy('contact_list')
