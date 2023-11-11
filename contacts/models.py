97011555794674bdc569f8d5668ff2347e2de233
VictoriaaTran <victoriatrnn@gmail.com>
2023-11-10 22:11:51 -0800

    add notes description to form

--- contacts/models.py
+++ contacts/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=20, null=False, unique=True, error_messages={'unique':"Contact with this name already exists."})
     email = models.EmailField(null=False, unique=True, error_messages={'unique':"Enter a valid email address."})
+    notes = models.TextField(max_length = 200, default="") #multiple lines of text
     created_time = models.DateTimeField(auto_now_add=True)
