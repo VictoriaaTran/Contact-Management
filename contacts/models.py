4f824db58fb4a7394b6fa607a455ca4b5f9fc8ea
VictoriaaTran <victoriatrnn@gmail.com>
2023-11-10 21:29:30 -0800

    added contact list page

--- contacts/models.py
+++ contacts/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class Contact(models.Model):
-    name = models.CharField(max_length=20, null=False, unique=True, error_messages={'unique':"Name has already been taken"})
-    email = models.EmailField(null=False, unique=True, error_messages={'unique':"Invalid email"})
+    name = models.CharField(max_length=20, null=False, unique=True, error_messages={'unique':"Contact with this name already exists."})
+    email = models.EmailField(null=False, unique=True, error_messages={'unique':"Enter a valid email address."})
     created_time = models.DateTimeField(auto_now_add=True)
