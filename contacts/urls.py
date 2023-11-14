719eaf6015f78945a198bf831a45588445e789b3
VictoriaaTran <victoriatrnn@gmail.com>
2023-11-14 15:17:28 -0800

    add contact update to urls

--- contacts/urls.py
+++ contacts/urls.py
@@ -4,10 +4,13 @@ from .views import (
     ContactListView,
     ContactCreateView,
     ContactDetailView,
+    ContactUpdateView,
 )
 
 urlpatterns = [
     path("", ContactListView.as_view(), name="contact_list"),
     path("create/", ContactCreateView.as_view(), name="contact_create"),
-    path("contacts/<int:pk>/", ContactDetailView.as_view(), name="contact_detail")
+    path("contacts/<int:pk>/", ContactDetailView.as_view(), name="contact_detail"),
+    path("contacts/<int:pk>/update/", ContactUpdateView.as_view(), name="contact_update"),
+
 ]
