57d4b312a7045446e8e121b893f6f34850469ec4
VictoriaaTran <victoriatrnn@gmail.com>
2023-11-14 16:50:27 -0800

    update database requirements

--- contacts/models.py
+++ contacts/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=20, null=False, unique=True, error_messages={'unique':"Contact with this name already exists."})
-    email = models.EmailField(null=False, unique=True, error_messages={'unique':"Enter a valid email address."})
-    notes = models.TextField(max_length = 200, default="") #multiple lines of text
+    email = models.EmailField(null=False, unique=True, error_messages={
+        'unique': "This email is already in use.",
+        'required': "Email field cannot be empty.",
+        'invalid': "Enter a valid email address."})
+    notes = models.TextField(max_length = 200, default="", blank="True") #multiple lines of text
     created_time = models.DateTimeField(auto_now_add=True)
